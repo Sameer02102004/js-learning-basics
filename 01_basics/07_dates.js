47b8f475da25f7a60d2579d6f1370f04fa7d809b
sameer singh sisodia <154669575+Sameer02102004@users.noreply.github.com>
2024-11-05 19:51:49 +0000

    done

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,16 @@
+let myDate = new Date()
+//console.log(myDate);
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2024 , 10 , 5)
+// console.log(myCreatedDate.toDateString());
+
+let myCreatedDate = new Date("2024-10-5")
+console.log(myCreatedDate.toLocaleDateString());
+
+
+
+
+
