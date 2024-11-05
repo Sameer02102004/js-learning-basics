dc4c20dd731dc03d5da7169c810df662c3ecf8cf
sameer singh sisodia <154669575+Sameer02102004@users.noreply.github.com>
2024-11-05 17:05:40 +0000

    done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,24 @@
+const score= 400
+
+const balance = new Number(100)
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+/*=====================Maths===========================*/
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6))
+// console.log(Math.floor(4.6))
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+
