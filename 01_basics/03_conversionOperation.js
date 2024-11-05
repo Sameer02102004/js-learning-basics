b299bf883af86b6e2f7a6129f44c6165be1dad94
sameer singh sisodia <154669575+Sameer02102004@users.noreply.github.com>
2024-11-05 11:49:50 +0000

    done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+// let score = "33"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(score);
+// console.log(valueInNumber);
+
+/*=======================================================================*/
+
+// let score = "33abc"
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(score);
+// console.log(valueInNumber);
+
+/*=======================================================================*/
+
+// let score = null
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(score);
+// console.log(valueInNumber);
+
+/*==========================================================================*/
+
+let someNumber = 33
+console.log(typeof someNumber);
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber);
+;
+
+
+
