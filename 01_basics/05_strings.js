f612e3407342f1f607af4fa1e0068839a510de4c
sameer singh sisodia <154669575+Sameer02102004@users.noreply.github.com>
2024-11-05 16:33:58 +0000

    done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+const name = "hitesh"
+const repoCount = 50
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// console.log(name[1]);
+/*================================================================*/
+
+// const gameName = new String("hiteshhc")
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+
+/* =============================================================*/
+// const newSringOne = "       hitesh     "
+// console.log(newSringOne);
+
+// console.log(newSringOne.trim());
+
+/*=================================================================*/
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace('%20','-'));
+
+/*================================================================*/
+const gameName = new String('hitesh-hc-com')
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
