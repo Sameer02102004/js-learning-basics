6975a0eb55f193ebd0f0f9100edb36ecb0ab022c
sameer singh sisodia <154669575+Sameer02102004@users.noreply.github.com>
2024-11-05 10:10:51 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+
+accountEmail = "hc@c.com"
+accountPassword = "212121"
+accountCity = "bengaluru"
+
+
+
+
+ console.table([accountId , accountEmail , accountPassword, accountCity, accountState])
+// if (true) {
+//     var x = 10;
+//     let y = 20;
+// }
+// console.log(x); // 10 (var is function or global scoped)
+// console.log(y); // ReferenceError: y is not defined (let is block scoped)
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict "; // by this, it will treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs , not browser thats why it will not work here
+let name = "sameer "
+let a = 24.4
+let isLoggedin = true
+
+
+console.log(typeof name); // string datatype
+
+// we can also check datatype by writing
+console.log(typeof "sameer");
+
+console.log(typeof a); // number datatype
+console.log(typeof isLoggedin); // boolean datatype
+console.log(typeof null);
+
+
+
